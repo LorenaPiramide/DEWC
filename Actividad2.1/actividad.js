24c8e95fa10ddca2e637554c44d3bce326de1a33
Lorena <230255@fppiramide.com>
2025-10-28 14:16:58 +0100

    Avanzando en el proyecto

--- Actividad2.1/actividad.js
+++ Actividad2.1/actividad.js
@@ -15,24 +15,26 @@ class Evento {
     }
 }
 
-// Cuando hagamos un evento y le pidamos al creador del evento que nos diga el nombre y el email de los invitados por el prompt, podremos pasarle los atributos juntos {nombre, email}
-
 function cargarAgenda(){
     const agendaLS = JSON.parse(localStorage.getItem("agenda")) || []; //Esto último siempre va a ser true, si no funciona lo primero, haz un array vacío
     const agenda = [...agendaLS].map((evento) => new Evento(evento));
 }
 
-/* function crearAgenda() {
+// Creo que con eso puedo mirar si hay una agenda y cargar la agenda ??
+function crearAgenda() {
     let agenda = [];
-} */
+}
+
+menuAgenda();
 
 function menuAgenda() {
+
     let opcion = prompt(
         "Bienvenido a la agenda, elige una opción.\n" +
         "1- Añadir evento.\n" +
         "2- Borrar evento.\n" +
         "3- Modificar evento.\n" +
-        "4- Desactivar alertas del evento.\n" +
+        "4- Desactivar alertas del evento.\n" + // con timeout
         "5- Borrar evento pasado.\n" +
         "6- Salir sin guardar.\n" +
         "7- Salir, guardar y mostrar agenda."
@@ -43,25 +45,24 @@ function menuAgenda() {
             addEvento();
             break;
         case '2':
-            // función
+            borrarEvento();
             break;
         case '3':
-            //funcion
+            modificarEvento();
             break;
         case '4':
-            // funcion
+            desactivarAlertas();
             break;
         case '5':
-            // funcion
+            borrarEventoAnterior();
             break;
         case '6':
-            // funcion
+            salirSinGuardar();
             break;
         case '7':
-            // funcion
+            guardarMostrarEvento();
             break;
     }
-
 }
 
 function addEvento() {
@@ -70,11 +71,16 @@ function addEvento() {
     let fechaHoraEvento = prompt("Dime la fecha y la hora del evento (AAAA-MM-DDTHH:MM).");
     let lugarEvento = prompt("Dime el lugar del evento.");
 
-    let numInvitadosEvento = prompt(parseInt("¿Cuántos invitados quieres en el evento?"));
+    let numInvitadosEvento = parseInt(prompt("¿Cuántos invitados quieres en el evento?"));
     const invitados = [];
     for (var i = 0; i < numInvitadosEvento; i++) {
         let nombreInvitado = prompt("Dime el nombre del invitado.");
         let emailInvitado = prompt("Dime el email del invitado.")
+
+        if (!emailInvitado.includes("@")) {
+            alert("No es un correo electrónico. Vuelve a intentarlo.");
+            emailInvitado = prompt("Dime el email del invitado.");
+        }
         
         let nuevoInvitado = new Invitado(nombreInvitado, emailInvitado);
 
@@ -83,11 +89,48 @@ function addEvento() {
 
     let nuevoEvento = new Evento(nombreEvento, textoEvento, fechaHoraEvento, lugarEvento, invitados);
 
+    // No sé cómo comprobar si es un evento vacío o no
     if (nuevoEvento != null) {
-        // Poner alerta de que se ha creado correctamente
+        alert("Se ha creado el evento correctamente.");
+        crearOtroEvento();
     } else {
-        // Poner alerta de que no se ha creado correctamente
+        alert("No se ha podido crear el evento.");
+        crearOtroEvento();
     }
-
     return nuevoEvento;
+}
+
+function crearOtroEvento() {
+    let confirmacion = prompt("¿Deseas volver a crear un nuevo evento? (s/n)");
+    if (confirmacion === "s") {
+        alert("Vas a crear un nuevo evento");
+        addEvento();
+    } else {
+        alert("Volviendo al menú de la agenda.");
+        menuAgenda();
+    }
+}
+
+function borrarEvento() {
+    let nombreEvento = prompt("Dime el nombre del evento que deseas borrar.");
+}
+
+function modificarEvento() {
+    let nombreEvento = prompt("Dime el nombre del evento que deseas eliminar.");
+}
+
+function desactivarAlertas() {
+    let nombreEvento = prompt("Dime el nombre del evento del que deseas desactivar las alertas.")
+}
+
+function borrarEventoAnterior() {
+
+}
+
+function salirSinGuardar() {
+
+}
+
+function guardarMostrarEvento() {
+    
 }
