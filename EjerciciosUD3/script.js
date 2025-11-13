8a06a05984777026b89f4fe1eec817b77bf77c9f
Lorena <230255@fppiramide.com>
2025-11-13 14:19:48 +0100

    Ejercicio 1

---
+++ EjerciciosUD3/script.js
@@ -0,0 +1,5 @@
+// Asigna un manejador de evento a los dos momentos de carga de la página (DOMContentLoaded, load,beforeunload) muestra el tipo de evento y el target.
+
+addEventListener("DOMContentLoaded", (event) => {
+    alert("Evento del DOMContentLoaded.");
+})
