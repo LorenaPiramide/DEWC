0d2ee616fd7e2b71b5dc4fc060e69f30878ca854
Lorena <230255@fppiramide.com>
2026-01-08 14:19:32 +0100

    Continuación

--- EjerciciosServidor/tareas.js
+++ EjerciciosServidor/tareas.js
@@ -1,7 +1,8 @@
 const URL = "http://100.49.134.235/3000";
 
 function pintarTareas() {
-
+    document.getElementByName("main").innerHTML = "<al>";
+    document.getElementsByName("")
 }
 
 function cargarTareas() {
