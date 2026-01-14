60b9180ef3575910b06cdbfb48227731fd38700e
Lorena <230255@fppiramide.com>
2026-01-14 11:47:18 +0100

    Continuación

--- EjerciciosServidor/tareas.js
+++ EjerciciosServidor/tareas.js
@@ -1,12 +1,14 @@
 const URL = "http://100.49.134.235/3000";
+document.addEventListener("DOMContentLoaded", cargarTareas);
 
 function pintarTareas() {
-    document.getElementByName("main").innerHTML = "<al>";
-    document.getElementsByName("")
+    document.querySelector("main").innerHTML = "";
+    // document.getElementByName("").innerHTML = "<ol>";
+    // document.getElementsByName("ol").innerHTML = "<li>";
+    document.getElementById("");
+    
 }
 
 function cargarTareas() {
 
 }
-
-//function
