7550ff0f0fe8877349221927a9e14fb5a6e1cb0e
Lorena <230255@fppiramide.com>
2026-01-15 13:57:54 +0100

    Pintar y cargar tareas

--- EjerciciosServidor/tareas.js
+++ EjerciciosServidor/tareas.js
@@ -1,14 +1,27 @@
-const URL = "http://100.49.134.235/3000";
+const URL = "http://100.49.134.235:3000/";
 document.addEventListener("DOMContentLoaded", cargarTareas);
 
-function pintarTareas() {
-    document.querySelector("main").innerHTML = "";
-    // document.getElementByName("").innerHTML = "<ol>";
-    // document.getElementsByName("ol").innerHTML = "<li>";
-    document.getElementById("");
-    
+function pintarTareas(tareas) {
+    const main = document.querySelector("main");
+    const listaTareas = document.createElement("ol");
+    main.append(listaTareas);
+    tareas.forEach(tarea => {
+        const lista = document.createElement("li");
+        lista.textContent = tarea.nombre;
+        lista.id = tarea.id;
+        const borrarTarea = document.createElement("button");
+        borrarTarea.textContent = "Eliminar";
+        lista.append(borrarTarea);
+        listaTareas.append(lista);
+    });
 }
 
 function cargarTareas() {
+    fetch(URL + "tareas")
+    .then(response => response.json())
+    .then(data => pintarTareas(data));
+}
 
+function borrarTarea(tarea) {
+    
 }
